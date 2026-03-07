5b05d01f88a0b91409c4e90400608c6ad7f36d47
minivalley01 <nppot.winas@gmail.com>
2026-03-08 02:50:05 +0700

    Update tasks.controller.ts

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -23,31 +23,43 @@ export class TasksController {
     
     constructor(private tasksService: TasksService) {}
 
+    @ApiOperation({ summary: 'Create tasks' })
+    @ApiResponse({ status: 200, description: 'Task created successfully' })
     @Post()
     createTask(@Body() dto: CreateTaskDto){
         return this.tasksService.createTask(dto);
     }
 
+    @ApiOperation({ summary: 'Get all tasks' })
+    @ApiResponse({ status: 200, description: 'Tasks retrieved successfully' })
     @Get()
     getTasks(@Query('projectId') projectId?: number){
         return this.tasksService.getAllTasks(projectId);
     }
 
+    @ApiOperation({ summary: 'Get task by id' })
+    @ApiResponse({ status: 200, description: 'Task retrieved successfully' })
     @Get(':id')
     getTask(@Param('id') id: string){
         return this.tasksService.getTaskById(id);
     }
 
+    @ApiOperation({ summary: 'Put task by id' })
+    @ApiResponse({ status: 200, description: 'Task updated successfully' })
     @Put(':id')
     updateTask(@Param('id') id: string, @Body() dto: UpdateTaskDto) {
         return this.tasksService.updatetask(id, dto);
     }
 
+    @ApiOperation({ summary: 'Patch task by id' })
+    @ApiResponse({ status: 200, description: 'Task updated partially' })
     @Patch(':id')
     patchTask(@Param('id') id: string, @Body() dto: UpdateTaskDto) {
         return this.tasksService.patchTask(id, dto);
     }
 
+    @ApiOperation({ summary: 'Delete task by id' })
+    @ApiResponse({ status: 200, description: 'Task deleted successfully' })
     @Delete(':id')
     deleteTask(@Param('id') id: string){
         return this.tasksService.deleteTask(id);
